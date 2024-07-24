b59b416fe8939b9444f4275ca9e84c9fbd88631e
Haowei Lee <haoweilee.tpe@gmail.com>
2024-07-24 16:29:37 +0800

    chore: set up db and add db migration

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+var ConnStr = FetchEnv("PGDATABASE")
+
+func main() {
+	fmt.Println(ConnStr)
+}
+
+func FetchEnv(key string) string {
+	value, exists := os.LookupEnv(key)
+
+	if !exists {
+		log.Fatalf("FATAL: Environment variable %s is not set!", key)
+	}
+
+	return value
+}
